06a870df582d8a86f68e8a2a225b18592f473292
Arron <beale86@msn.com>
2023-08-17 07:22:26 +0000

    Replace products with new products, deploy to gh pages

--- src/components/Shop/Products.js
+++ src/components/Shop/Products.js
@@ -4,15 +4,33 @@ import classes from './Products.module.css';
 const DUMMY_PRODUCTS = [
   {
     id: 'p1',
-    price: 6,
-    title: 'My First Book',
-    description: 'The first book I ever wrote',
+    price: 600,
+    title: 'O2 Concentrator',
+    description: 'Standard flow (1-5LPM)',
   },
   {
     id: 'p2',
-    price: 5,
-    title: 'My Second Book',
-    description: 'The second book I ever wrote',
+    price: 800,
+    title: 'High Flow Concentrator',
+    description: 'High Flow (9LPM)',
+  },
+  {
+    id: 'p3',
+    price: 800,
+    title: 'Low Flow Concentrator',
+    description: 'Low Flow',
+  },
+  {
+    id: 'p4',
+    price: 900,
+    title: 'Portable Oxygen Concentrator (POC)',
+    description: 'Compact portable oxygen concentrator',
+  },
+  {
+    id: 'p5',
+    price: 1000,
+    title: 'Transportable Oxygen Concentrator (TOC)',
+    description: 'transportable oxygen concentrator',
   },
 ];
 
